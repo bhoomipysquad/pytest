8d72996b3194de9277e9c9f3f681b2f5e0f81075
Bhoomi Darji <bhoomi.pysquad@gmail.com>
2025-02-21 12:41:22 +0530

    21/02

--- Cases_of_pytest/diff_browser_in_parameter.py
+++ Cases_of_pytest/diff_browser_in_parameter.py
@@ -6,12 +6,12 @@ from selenium.webdriver.common.by import By
 @pytest.fixture(params=["chrome","firefox","edge"])
 def initialize_driver(request):
     if request.param == "chrome":
-        driver=webdriver.chrome()
+        driver= webdriver.Chrome()
 
     elif request.param == "firefox":
-        driver=webdriver.firefox()
+        driver=webdriver.Firefox()
 
     elif request.param == "edge":
-         driver=webdriver.edge()
+         driver=webdriver.Edge()
 
 
